eca1bbfc107b23be0eb7aa023c18b5a772a56ccc
Jia Li <jellybean482@gmail.com>
2024-03-25 20:23:32 -0400

    release 2

--- src/main/Java/com/jia/csv/CSVReader.java
+++ src/main/Java/com/jia/csv/CSVReader.java
@@ -9,11 +9,12 @@ import java.util.AbstractMap.SimpleEntry;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /*
- * Assumptions made:
+ * Assumptions:
  * - Field names in the first line are unique
  * - Column order is not important
  * - Row order is not important
@@ -21,60 +22,21 @@ import java.util.stream.IntStream;
  * - Quoted strings, either for field names or values, are stored in the map with quotes stripped while leading and trailing spaces are preserved
  * 
  * Tested with 
+ * - CVSReaderUnitTest.java
  * - wacky.csv: contains some wacky cases (quotes, commas and leading/trailing spaces in field names and values, empty lines, missing cells)
  * - HMEQ.csv: supplied by assigner
  * 
  * Time spent on solution: 3/18/2024 7-9 pm; 3/19/2024 from 9-11 pm.
  * 
- * 
- * Overview: This is an exercise to explore your competency in the following areas:
- *   • modern java language features like Streams, Predicates, Function objects etc.
- *   • algorithmic thinking and modular design in terms of classes and methods.
- *   • general awareness of constraints when processing large files of data.
- *
- * Requirements:
- *   • Write a java program in the form of a java Stream pipeline to read a file in CSV (comma separated values) format (example attached).
- *     The first line of the file contains the names of the fields.
- *     The other lines contain values corresponding to the names in the first line.
- *
- *   • The program should read each of the other lines into a Map object and print its contents. Please ensure all lines are read even when some lines have errors.
- *
- *   • Every entry in the Map should contain:
- *   • Key - a string containing the field name (from the first row)
- *   • Value - the corresponding value (from the current row)
- *
- *   • A Value could be a String (sequence of characters), Double (floating point number) or Integer (no decimal point) and should be read as an Object of the appropriate type.
- *   • Null values are possible as are empty strings.
- *   • String values may be quoted or unquoted and may contain commas.
- *   • Please ignore leading and trailing spaces for numerical values and unquoted string values.
- *   • Leading and trailing spaces for quoted string values are significant and must not be ignored.
- *
- * Acceptance criteria:
- *   • Must read the lines of the file as a Stream. The input file may have millions of lines of data.
- *   
- *   • Use of explicit loops like while, for etc. are not permitted. Please use Stream functions like foreach, filter, map, join etc. to iterate and transform.
- *     This will be used to evaluate the competency in the modern java language features.
- *
- *   • There are many ways to parse data in java. Please choose an appropriate mechanism and describe why you chose it.
- *
- *   • If you want to make improvements or extend your code but do not have time, leave a //TODO comment in your code along with a description of what you would have done given time, and why.
- *
- * Bonus points:
- *   • Provide a solution that makes use of parallelism in processing the rows.
- *
- * Please limit the time you work on your solution to four hours.
- * Please commit the solution to a public git repository of your choice (eg, github), and let us know the location of the repository (preferably one day prior to your scheduled interview).
- *
- * Be prepared to:
- *   • Present your solution and walk through how you designed, implemented, and tested the program. During the technical interview, we may ask you to share your screen and show the program running.
- *   • Discuss any assumptions you made and challenges you encountered.
  */
-public class CSVReader {
+final public class CSVReader {
     private static final String COMMA_DELIMITER = ",";
+    private static final Pattern COMMA_PATTERN = Pattern.compile(COMMA_DELIMITER); // precompile for repeated use
     private static final String COMMA_IGNORE_QUOTED = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
+    private static final Pattern COMMA_IGNORE_QUOTED_PATTERN = Pattern.compile(COMMA_IGNORE_QUOTED); // precompile for repeated use
     private static final int SERIAL_READ = 0;
     private static final int PARALLEL_READ = 1;
-    
+
     private String[] names;
 
     public void read(String filePath, int method) throws IOException {
@@ -86,20 +48,21 @@ public class CSVReader {
             System.out.println("File " + filePath + " can not be found.");
             throw e;
         }
-        
+
         try {
             // read the header line
             String nameLine = reader.readLine();
             names = split(nameLine);
-            names = Arrays.stream(names).map(n->parseString(n)).toArray(size -> new String[size]);
-            
+            names = Arrays.stream(names).map(n -> parseString(n)).toArray(size -> new String[size]);
+
             // read and print the value lines
+            // No need to return anything because we are done printing
             switch (method) {
             case SERIAL_READ:
-                serialRead(reader);
+                reader.lines().map(lineToMap).forEach(System.out::println);
                 break;
             case PARALLEL_READ:
-                parallelRead(reader);
+                reader.lines().parallel().map(lineToMap).forEach(System.out::println);
                 break;
             default:
                 break;
@@ -108,26 +71,18 @@ public class CSVReader {
             System.out.println("Problem encountered reading file " + filePath);
             throw e;
         } finally {
-            if ( reader != null ) reader.close();
-        }        
+            if (reader != null)
+                reader.close();
+        }
     }
 
-    // No need to return anything because we are done printing
-    private void serialRead(BufferedReader reader)
-    {
-        reader.lines().map(lineToMap).forEach(System.out::println);
-    }
-    
-    // No need to return anything because we are done printing
-    private void parallelRead(BufferedReader reader)
-    {
-        reader.lines().parallel().map(lineToMap).forEach(System.out::println);
-    }
-    
     // Continue despite error
     private Function<String, Map<String, Object>> lineToMap = (line) -> {
         try {
             String[] values = split(line);
+
+            if (values.length > names.length)
+                values = Arrays.copyOf(values, names.length);
 
             Object[] valueObjects = Arrays.stream(values).map(v -> parseValue(v)).toArray(size -> new Object[size]);
 
@@ -143,16 +98,16 @@ public class CSVReader {
         }
     };
 
-    // Only pass through regex if there's a double quote (") in the string because regex matching is expensive
-    private String[] split(String line) {
-        return line.contains("\"") ? line.split(COMMA_IGNORE_QUOTED) : line.split(COMMA_DELIMITER); 
+    // Only pass through COMMA_IGNORE_QUOTED if there's a double quote (") in the string because regex matching is expensive
+    public static String[] split(String line) {
+        return line.contains("\"") ? COMMA_IGNORE_QUOTED_PATTERN.split(line) : COMMA_PATTERN.split(line);
     }
-    
-    private Object parseValue(String s) {
-        if (s == null )
+
+    public static Object parseValue(String s) {
+        if (s == null)
             return "";
         s = s.trim();
-        if ( s == "" )
+        if (s == "")
             return "";
         Object o;
         if ((o = parseInteger(s)) != null)
@@ -162,7 +117,7 @@ public class CSVReader {
         return parseString(s);
     }
 
-    private Integer parseInteger(String s) {
+    public static Integer parseInteger(String s) {
         try {
             return Integer.valueOf(Integer.parseInt(s));
         } catch (NumberFormatException e) {
@@ -170,7 +125,7 @@ public class CSVReader {
         }
     }
 
-    private Double parseDouble(String s) {
+    public static Double parseDouble(String s) {
         try {
             return Double.valueOf(Double.parseDouble(s));
         } catch (NumberFormatException e) {
@@ -178,7 +133,7 @@ public class CSVReader {
         }
     }
 
-    private String parseString(String s) {
+    public static String parseString(String s) {
         if (s.charAt(0) == '\"' && s.charAt(s.length() - 1) == '\"')
             s = s.substring(1, s.length() - 1);
         return s;
@@ -186,14 +141,14 @@ public class CSVReader {
 
     public static void main(String[] args) {
         CSVReader reader = new CSVReader();
-        
+
         try {
             // Serial read
             System.out.println("=================Serial read");
             long serialStart = System.currentTimeMillis();
             reader.read(args[0], SERIAL_READ);
             long serialEnd = System.currentTimeMillis();
-            
+
             System.out.println("\n=================Parallel read");
             long parallelStart = System.currentTimeMillis();
             reader.read(args[0], PARALLEL_READ);
---
+++ src/test/Java/com/jia/csv/test/CSVReaderUnitTest.java
@@ -0,0 +1,101 @@
+package com.jia.csv.test;
+
+import static org.junit.Assert.assertTrue;
+
+import com.jia.csv.CSVReader;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CSVReaderUnitTest {
+
+    @Test
+    public void splitRegular() {
+        String[] expected = new String[] {"regular", "string"};
+        StringBuffer sb = new StringBuffer(String.join(",", expected));
+        String[] actual = CSVReader.split(sb.toString());
+        Assert.assertArrayEquals(expected, actual);
+    }
+    
+    @Test
+    public void splitIgnoreQuotedComma() {
+        String[] expected = new String[] {"string", "\",\"with comma"};
+        StringBuffer sb = new StringBuffer(String.join(",", expected));
+        String[] actual = CSVReader.split(sb.toString());
+        Assert.assertArrayEquals(expected, actual);
+    }
+    
+    @Test
+    public void splitContainQuotes() {
+        String[] expected = new String[] {"\" quoted \"", "string"};
+        StringBuffer sb = new StringBuffer(String.join(",", expected));
+        String[] actual = CSVReader.split(sb.toString());
+        Assert.assertArrayEquals(expected, actual);
+    }
+    
+    @Test
+    public void splitContainUnmatchedQuote() {
+        String[] expected = new String[] {"\" quoted ", "string"};
+        StringBuffer sb = new StringBuffer(String.join(",", expected));
+        String[] actual = CSVReader.split(sb.toString());
+        Assert.assertArrayEquals(expected, actual);
+    }
+    
+    @Test
+    public void parseValue() {
+        String expected = "string";
+        Object actual = CSVReader.parseValue(expected);
+        assertTrue(actual instanceof String);
+        Assert.assertEquals(expected, (String)actual);
+    }
+    
+    @Test
+    public void parseValueStringLeadingSpaces() {
+        String expected = "string";
+        StringBuffer sb = new StringBuffer("   ");
+        sb.append(expected);
+        Object actual = CSVReader.parseValue(expected);
+        assertTrue(actual instanceof String);
+        Assert.assertEquals(expected, (String)actual);
+    }
+    
+    @Test
+    public void parseValueStringTrailingSpaces() {
+        String expected = "string";
+        StringBuffer sb = new StringBuffer(expected);
+        sb.append("   ");
+        Object actual = CSVReader.parseValue(expected);
+        assertTrue(actual instanceof String);
+        Assert.assertEquals(expected, (String)actual);
+    }
+    
+    @Test
+    public void parseValueInteger() {
+        int expected = 123;
+        Object actual = CSVReader.parseValue(String.valueOf(expected));
+        assertTrue(actual instanceof Integer);
+        Assert.assertEquals(expected, ((Integer)actual).intValue());
+    }
+    
+    @Test
+    public void parseValueDouble() {
+        double expected = 123.456;
+        Object actual = CSVReader.parseValue(String.valueOf(expected));
+        assertTrue(actual instanceof Double);
+        Assert.assertEquals(expected, ((Double)actual).doubleValue(), 0.001);
+    }
+    
+    @Test
+    public void parseInteger() {
+        int expected = 123;
+        Integer actual = CSVReader.parseInteger(String.valueOf(expected));
+        Assert.assertEquals(expected, actual.intValue());
+    }
+    
+    @Test
+    public void parseDouble() {
+        double expected = 123.456;
+        Double actual = CSVReader.parseDouble(String.valueOf(expected));
+        Assert.assertEquals(expected, actual.doubleValue(), 0.001);
+    }
+}
